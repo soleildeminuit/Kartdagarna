2cef1411d7839623662492aaa58dbc53c41ae521
soleildeminuit <anders.elias@gmail.com>
2022-04-03 23:03:50 +0200

    Add ggplot2.

--- workshop3.R
+++ workshop3.R
@@ -14,7 +14,7 @@ pxdf <- readRDS("data/pxdf.rds")
 
 deso_karlstad <- pxdf %>% 
   filter(grepl("^\\d{4}[A-C]\\d{4}$", region) == TRUE,
-         substr(region, 1, 4) == "1780",
+         substr(region, 1, 4) == "1784",
          ålder == "totalt", kön == "totalt") %>% 
   select(-ålder, -kön) %>% 
   rename(deso = region) %>% 
@@ -31,13 +31,44 @@ ci <- classIntervals(deso_karlstad$bef_antal, n = 5, style = "jenks")
 deso_karlstad <- deso_karlstad %>%
   mutate(breaks = cut(bef_antal, ci$brks, include.lowest = T, dig.lab=10))
 
+
+gg <- ggplot(deso_karlstad) + 
+  geom_sf(aes(fill=bef_antal)) +
+  # scale_fill_gradient2(low = "darkred", mid = "grey85", high = "darkgreen", midpoint = mean(deso_karlstad$bef_antal)) +
+  ggthemes::theme_few()
+
+gg
+
 gg <- ggplot(deso_karlstad) + 
   geom_sf(aes(fill=breaks)) +
   # scale_fill_gradient2(low = "darkred", mid = "grey85", high = "darkgreen", midpoint = mean(deso_karlstad$bef_antal)) +
-  scale_fill_brewer(palette = "RdYlBu") +
+  scale_fill_brewer(palette = "RdYlBu") + theme_void()
   ggthemes::theme_few()
 
 gg
 
 plot_gg(gg,multicore = TRUE,width=5,height=5,scale=250,windowsize=c(1400,866),
         zoom = 0.55, phi = 30)
+
+###########################################################################
+
+# Skapa PowerPoint-presentation
+library(officer)
+
+p_dml <- rvg::dml(ggobj = gg)
+
+# initiera ppt-slide
+officer::read_pptx() %>%
+  # lägg till
+officer::add_slide() %>%
+  # ange objekt
+officer::ph_with(p_dml, ph_location_fullsize()) %>%
+  # exportera
+base::print(
+  target = here::here(
+    "slides",
+    "",
+    "",
+    "karta_ppt_demo.pptx"
+  )
+)
